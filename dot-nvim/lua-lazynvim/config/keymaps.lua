eaf929d2093af254405fe41527f7604c8f16c5a2
diegopatas <redediniz@gmail.com>
2024-10-15 21:27:38 -0300

    refac: restructure dot-files folders

---
+++ dot-nvim/lua-lazynvim/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', ':')
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('i', 'jj', '<ESC>')
+-- vim.keymap.set('i', '<C-j>', '<C-\\><C-n>: call search(\'[>)\]}\"\'\']', 'W')<CR>a')
